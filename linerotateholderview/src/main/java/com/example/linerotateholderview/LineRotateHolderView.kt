0843973ee87d21ed58c0e2bf20cf538e30450868
Anwesh43 <anweshthecool0@gmail.com>
2021-05-17 00:52:23 +0530

    adding Canvas extension methods

--- linerotateholderview/src/main/java/com/example/linerotateholderview/LineRotateHolderView.kt
+++ linerotateholderview/src/main/java/com/example/linerotateholderview/LineRotateHolderView.kt
@@ -31,3 +31,36 @@ fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.drawLineRotateHolder(scale : Float, w : Float, h : Float, paint : Paint) {
+    val size : Float = Math.min(w, h) / sizeFactor
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(lines, parts)
+    val sf2 : Float = sf.divideScale(lines + 1, parts)
+    val sqSize : Float = Math.min(w, h) / boxSizeFactor
+    save()
+    for (j in 0..(lines - 1)) {
+        save()
+        translate(0f, h / 2)
+        rotate(180f * sf.divideScale(j, parts))
+        save()
+        translate(-size * j, 0f)
+        drawLine(0f, 0f, -size, 0f, paint)
+        restore()
+        restore()
+    }
+    save()
+    translate((size * lines) / 2, (h / 2) * sf2)
+    drawRect(RectF(-sqSize / 2, 0f, sf1 * sqSize * 0.5f, sqSize), paint)
+    restore()
+    restore()
+}
+
+fun Canvas.drawLRHNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    drawLineRotateHolder(scale, w, h, paint)
+}
